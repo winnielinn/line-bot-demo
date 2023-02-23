3a9ac832882f7c10bd1fdbff5d88e37c4b60de5d
winnielinn <yaysa903@gmail.com>
2023-02-23 23:04:00 +0800

    feat: create flex messages

---
+++ src/messages/flex-message.ts
@@ -0,0 +1,79 @@
+import { FlexBox, FlexImage, FlexMessage } from '@line/bot-sdk';
+
+const menuImage: FlexImage = {
+  type: 'image',
+  url: 'https://play-lh.googleusercontent.com/kCuoLGcYqdmZN_TvKVYrUjuF2C8uua2rfY83anNJw7YGzijReQc3yTlsqzvMdxs03IM=w240-h480-rw',
+  size: 'full',
+  aspectRatio: '20:13',
+  aspectMode: 'cover',
+  action: {
+    type: 'uri',
+    label: 'Browse',
+    // TODO: 替換成 github
+    uri: 'http://linecorp.com/',
+  },
+};
+
+const menuTittle: FlexBox = {
+  type: 'box',
+  layout: 'vertical',
+  contents: [
+    {
+      type: 'text',
+      text: '現在你想來點 ...',
+      weight: 'bold',
+      size: 'xl',
+    },
+  ],
+};
+
+const menuOptions: FlexBox = {
+  type: 'box',
+  layout: 'vertical',
+  spacing: 'sm',
+  contents: [
+    {
+      type: 'button',
+      height: 'sm',
+      action: {
+        type: 'postback',
+        label: '隨機中文笑話',
+        data: 'randomChineseJoke',
+        text: '隨機中文笑話',
+      },
+    },
+    {
+      type: 'button',
+      height: 'md',
+      action: {
+        type: 'postback',
+        label: '隨機英文笑話',
+        data: 'randomEnglishJoke',
+        text: '隨機英文笑話',
+      },
+    },
+    {
+      type: 'button',
+      style: 'link',
+      height: 'sm',
+      action: {
+        type: 'uri',
+        label: '回饋表單',
+        // TODO: 改為 Google 表單
+        uri: 'http://linecorp.com',
+      },
+    },
+  ],
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const menuMessage: FlexMessage = {
+  type: 'flex',
+  altText: 'This is a joke menu.',
+  contents: {
+    type: 'bubble',
+    hero: menuImage,
+    body: menuTittle,
+    footer: menuOptions,
+  },
+};
