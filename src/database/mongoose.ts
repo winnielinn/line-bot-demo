361af3599f6a22987d845228f17df185b235f6d8
winnielinn <yaysa903@gmail.com>
2023-03-09 23:17:53 +0800

    feat: update mongodb connection

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import { MongoClient, Db } from 'mongodb';
+import * as mongoDB from 'mongodb';
 import Logger from '../util/logger';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,12 +9,11 @@ if (process.env.NODE_ENV !== 'production') {
 const logger = Logger.getLogger('mongodb-connection');
 const uri: string = process.env.MONGODB_URI || '';
 
-async function connectToDatabase(): Promise<Db> {
+async function connectToDatabase() {
   try {
-    const client: MongoClient = await MongoClient.connect(uri);
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(uri);
+    await client.connect();
     logger.info('MongoDB connected successfully');
-    const database: Db = client.db();
-    return database;
   } catch (err) {
     logger.error(`MongoDB connected failed. Error: ${err}`);
     process.exit(1);
